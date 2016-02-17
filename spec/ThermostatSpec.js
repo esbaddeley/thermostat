8d799a16e59fb3135f615cc0e0997c737930367f
chris <chris@chris-MacBook-Pro.local>
2016-02-17 17:25:27 +0000

    maxtemp power on

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -2,25 +2,35 @@ describe ('Thermostat', function(){
   thermostat1 = new Thermostat();
 
 
-  it ('sets thermostat temperature to 20 degrees', function(){
-    expect(thermostat1.temperature).toEqual(20);
-  });
+    it ('sets thermostat temperature to 20 degrees', function(){
+      expect(thermostat1.temperature).toEqual(20);
+    });
 
-  it ('increases the temperature by 1 degree', function (){
-    thermostat1.UpButton();
-    expect(thermostat1.temperature).toEqual(21);
-  });
+    it ('increases the temperature by 1 degree', function (){
+      thermostat1.UpButton();
+      expect(thermostat1.temperature).toEqual(21);
+    });
 
-  it ('decreases the temperature by 1 degree', function (){
-    thermostat1.DownButton();
-    expect(thermostat1.temperature).toEqual(20);
-  });
+    it ('decreases the temperature by 1 degree', function (){
+      thermostat1.DownButton();
+      expect(thermostat1.temperature).toEqual(20);
+    });
 
 
-  it ('fails when reaches minimum temp', function() {
-    for(var i = 20; i > 10; i--){
-    thermostat1.DownButton();
-  }
+    it ('fails when reaches minimum temp', function() {
+      for(var i = 20; i > 10; i--){
+      thermostat1.DownButton();
+    }
     expect(thermostat1.DownButton()).toEqual(this.MIN_TEMP);
     });
+
+    it ('sets maxtemp when power saving mode is on', function(){
+        expect(thermostat1.isPowerSaving()).toEqual(true);
+    });
+
+    it ('sets maxtemp when power saving mode is on', function(){
+        thermostat1.temperature = 25;
+        expect(thermostat1.UpButton()).toEqual("Power saving, max temp reached");
+    });
+
   });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,9 +1,12 @@
 function Thermostat (){
   this.temperature = 20;
   this.MIN_TEMP = 10;
+  this.Power = false;
+  this.MaxPowerOn = 25;
 }
 
 Thermostat.prototype.UpButton = function(){
+    if (this.temperature === this.MaxPowerOn && this.Power === true) return ("Power saving, max temp reached");
     this.temperature +=1;
     //console.log(this.temperature);
 };
@@ -11,4 +14,9 @@ Thermostat.prototype.UpButton = function(){
 Thermostat.prototype.DownButton = function(){
    if (this.temperature < this.MIN_TEMP) return this.MIN_TEMP;
    this.temperature -=1;
+};
+
+
+Thermostat.prototype.isPowerSaving = function (){
+  return this.Power = true;
 };
