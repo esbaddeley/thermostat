70fa2ea46949c2c988007920c565c55344086014
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-02-17 16:27:32 +0000

    Added min_temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,18 +1,26 @@
 describe ('Thermostat', function(){
   thermostat1 = new Thermostat();
 
-});
 
-it('sets thermostat temperature to 20 degrees', function(){
-  expect(thermostat1.temperature).toEqual(20);
-});
+  it ('sets thermostat temperature to 20 degrees', function(){
+    expect(thermostat1.temperature).toEqual(20);
+  });
 
-it ('increases the temperature by 1 degree', function (){
-  thermostat1.UpButton();
-  expect(thermostat1.temperature).toEqual(21);
-});
+  it ('increases the temperature by 1 degree', function (){
+    thermostat1.UpButton();
+    expect(thermostat1.temperature).toEqual(21);
+  });
 
-it ('decreases the temperature by 1 degree', function (){
-  thermostat1.DownButton();
-  expect(thermostat1.temperature).toEqual(20);
-});
+  it ('decreases the temperature by 1 degree', function (){
+    thermostat1.DownButton();
+    expect(thermostat1.temperature).toEqual(20);
+  });
+
+
+  it ('fails when reaches minimum temp', function() {
+    for(var i = 20; i > 10; i--){
+    thermostat1.DownButton();
+  }
+    expect(thermostat1.DownButton()).toEqual(this.MIN_TEMP);
+    });
+  });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,6 @@
 function Thermostat (){
   this.temperature = 20;
+  this.MIN_TEMP = 10;
 }
 
 Thermostat.prototype.UpButton = function(){
@@ -8,6 +9,6 @@ Thermostat.prototype.UpButton = function(){
 };
 
 Thermostat.prototype.DownButton = function(){
-  this.temperature -=1;
-
+   if (this.temperature < this.MIN_TEMP) return this.MIN_TEMP;
+   this.temperature -=1;
 };
