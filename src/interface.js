81e5d78b2e00ff067aabec72c9a1374ab53a5c0f
Mic <mic.cassano@gmail.com>
2016-02-18 16:00:17 +0000

    Add city weather functionality

--- src/interface.js
+++ src/interface.js
@@ -3,6 +3,7 @@ var thermostat;
 $( document ).ready(function() {
 
     thermostat = new Thermostat();
+    var city;
     updateTemperature();
 
     $('#temperature-up').click(function() {
@@ -25,9 +26,23 @@ $( document ).ready(function() {
       $('#power-saving-status').text(thermostat.power ? "on" : "off");
     });
 
+    $('#select-city').submit(function () {
+      event.preventDefault();
+      city = $('#current-city').val();
+      updateCity();
+    });
+
     function updateTemperature() {
-      $('#temperature').text(thermostat.temperature);
+      $('#temperature').text(thermostat.temperature + ' \u2103');
       $('body').attr('class', thermostat.energyUsage());
     }
 
+    function updateCity() {
+      var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+      var token = '&appid=01f338575399a50d22ec5f76e47f4f1a';
+      var units = '&units=metric';
+      $.get(url + token + units, function (data) {
+        $("#city-temperature").text(data.main.temp + ' \u2103');
+      })
+    }
 });
