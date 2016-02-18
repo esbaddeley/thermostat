afff27f3afd58ec198fd87703fdfdfd795fcc821
ZeshanRasul <zeshanrasul90@gmail.com>
2016-02-18 15:39:54 +0000

    Implemented weather API

--- js/thermostatUi.js
+++ js/thermostatUi.js
@@ -1,8 +1,7 @@
 $( document ).ready(function() {
 
   var thermostat = new Thermostat();
-  var tempColour;
-
+  var tempColour, cityName, temp, city;
   // $('#temperature').thermometer( {
 	// 	height: 300,
 	// 	textColour: '#ffffff',
@@ -52,6 +51,32 @@ $( document ).ready(function() {
     thermostat.switchPowerModeOff();
     $('#power-saving-status').text(thermostat.powerMode());
   });
+
+
+  // $.ajax({
+  //   url: http://api.openweathermap.org/data/2.5/forecast/weather?q=Londonid=524901&APPID=ca6f6c4482e27abd7c68f8d5e303960f
+  //   data:
+  //   success:
+  //   dataType:
+  // });
+  $("#userInput").on("submit", function( event ){
+    event.preventDefault();
+    city = $( this ).serialize() ;
+    $.get('http://api.openweathermap.org/data/2.5/forecast/weather?' + city + '&APPID=ca6f6c4482e27abd7c68f8d5e303960f&units=metric', function( report ) {
+      console.log(report);
+      console.log(report.city.name);
+      console.log(report.list[0].main.temp);
+      console.log(report.list[0].weather[0].main);
+      cityName = report.city.name;
+      temp = report.list[0].main.temp;
+      weather = report.list[0].weather[0].main;
+      $('#cityName').text(cityName);
+      $('#temp').text(temp);
+      $('#forecast').text(weather);
+    });
+  })
+  console.log(city)
+
 
 
 
